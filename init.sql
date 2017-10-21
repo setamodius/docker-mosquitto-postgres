193b1cd5a42666a271d82e71c64e9a54a71938d9
Kerem Bilgiçer <krmbil@gmail.com>
2017-10-21 21:27:12 +0300

    docker updated

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS users;
 
-create table users (id SERIAL PRIMARY KEY, username VARCHAR(25) NOT NULL, password VARCHAR(128) NOT NULL, super smallint NOT NULL DEFAULT 0);
+create table users (id SERIAL PRIMARY KEY, username VARCHAR(25) NOT NULL, password VARCHAR(128) NOT NULL, name VARCHAR(128) NOT NULL, privilege int NOT NULL, super smallint NOT NULL DEFAULT 0);
 CREATE UNIQUE INDEX users_username ON users (username);
 
-insert into users(username, password, super) values('admin', 'PBKDF2$sha1$98$XaIs9vQgmLujKHZG4/B3dNTbeP2PyaVKySTirZznBrE=$2DX/HZDTojVbfgAIdozBi6CihjWP1+akYnh/h9uQfIVl6pLoAiwJe1ey2WW2BnT+', 1);
+insert into users(username, password, name, privilege, super) values('master', 'PBKDF2$sha256$10000$YZsCjjHK2HdNJqTC$meVp4QPmpOl0ZF+RIRMzEAxbwO9ylD+S', 'Master User', 252, 1);
 
 
 DROP TABLE IF EXISTS acls;

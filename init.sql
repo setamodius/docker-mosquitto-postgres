7880459a6ce377d476141d96e2bcb8942a6c8a30
Kerem Bilgiçer <setamodius@users.noreply.github.com>
2018-01-22 14:37:13 +0300

    master password added

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users;
 create table users (id SERIAL PRIMARY KEY, username VARCHAR(25) NOT NULL, password VARCHAR(128) NOT NULL, name VARCHAR(128) NOT NULL, privilege int NOT NULL, super smallint NOT NULL DEFAULT 0);
 CREATE UNIQUE INDEX users_username ON users (username);
 
-insert into users(username, password, name, privilege, super) values('master', 'PBKDF2$sha256$10000$YZsCjjHK2HdNJqTC$meVp4QPmpOl0ZF+RIRMzEAxbwO9ylD+S', 'Master User', 252, 1);
+insert into users(username, password, name, privilege, super) values('master', 'PBKDF2$sha256$10000$3YA/d8uocGb7X6FP$xXqfJgnX7H651ZQTwpVnwrg2ODIOvla+', 'Master User', 252, 1);
 
 
 DROP TABLE IF EXISTS acls;
